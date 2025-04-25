176ba2ef88937a0c7bdd1bc344f60826220c7610
walwell1907 <145315205+walwell1907@users.noreply.github.com>
2025-04-25 12:54:09 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+<div class="container-fluid wrap bg-warning" style="padding: 2%">
+	<div class="row">
+		<div align="center">
+			<h1><b>TODO LIST APP</b></h1>
+			<p>Nama Peserta - No Peserta</p>
+		</div>
+	</div>
+</div>
